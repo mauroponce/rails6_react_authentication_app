aadbb4f8289b1200b6da6335451d0cd1be18a52b
Mauro Ponce <mgp1989@gmail.com>
2022-12-01 21:30:20 -0300

    Registrations endpoint

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,1 +1,1 @@
-Rails.application.config.session_store :cookie_store, key: "_authentication_app", domain: "your-production-app-here.com"
+Rails.application.config.session_store :cookie_store, key: "_authentication_app"
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  resources :sessions, only: [:create]
+  resources :sessions, only: [:create] do
+    collection do
+      get :logged_in
+      delete :logout
+    end
+  end
+  resources :registrations, only: [:create]
 end
