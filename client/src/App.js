c3d7e19a5540e49a02d6c8207f8ac94568773546
Mauro Ponce <mgp1989@gmail.com>
2022-12-06 19:45:28 -0300

    Add react-router-dom v6 and routes definition

--- client/src/App.js
+++ client/src/App.js
@@ -1,33 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import {Routes, Route} from 'react-router-dom';
+import Home from './components/Home';
+import Dashboard from './components/Dashboard';
 
-async function checkLoggedIn(event) {
-  event.preventDefault();
-  const response = await fetch('http://localhost:4000/sessions/logged_in')
-  response.json().then((data) => {
-    alert('Logged in: ' + data.logged_in)
-  })  
-}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <button onClick={checkLoggedIn}>
-            Is Logged in?
-          </button>
-        </a>
-      </header>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/dashboard' element={<Dashboard/>} />
+      </Routes>
     </div>
   );
 }
